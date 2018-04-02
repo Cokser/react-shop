f4ae8181c7b25900982615f1f493d3e6fabc7d2b
Valeriy Khupeniya <thecokser@gmail.com>
2018-04-02 08:54:23 +0000

    Pure React Version of Shop App

--- src/App/HomePage/HomePage.js
+++ src/App/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import ProductsComponent from '../Products/Products';
 
 const HomePage = (props) => (
 	<div className="col-10 mx-auto">
-		<h1 className="text-center">Finally at Home!</h1>
+		<h1 className="text-center">Finally You Back to me!</h1>
 		<ProductsComponent count="4" mode="list" history={props.history} />
 	</div>
 );
